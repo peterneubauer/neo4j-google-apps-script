13073eb72db18c3406e7c51b4e62b4eaf0d999c9
Peter Neubauer <peter@neubauer.se>
2011-12-13 22:28:07 +0100

    rewamping to load better

--- neo4j-gas-driver.js
+++ neo4j-gas-driver.js
@@ -1,27 +1,30 @@
-function cypher(query, uri) {
-  var response = UrlFetchApp.fetch(uri, { "method" : "POST","payload" : query});
-  return response.getContentText();
-}
+var neo4j = {
 
-function cell(str, row, column) {
-  var result=Utilities.jsonParse(str);
-  var col=indexOf(result['columns'],column);
-  return result['data'][row][col];
-}
-
-function indexOf(ary, data) {
-  for (var i=0; i<ary.length;i++) {
-     if ( data == ary[i]) return i;
+  cypher: function(query, uri) {
+    var response = UrlFetchApp.fetch(uri, { "method" : "POST","payload" : query});
+    return response.getContentText();
   }
-  return -1;
-}
 
-//utility functions
+  cell: function(str, row, column) {
+    var result=Utilities.jsonParse(str);
+    var col=indexOf(result['columns'],column);
+    return result['data'][row][col];
+  }
 
-function dateToString(date) {
-  Logger.log("date: "+ datum);
-  var doc = SpreadsheetApp.getActiveSheet();
-  var datum = date;
-  Logger.log(datum);
-  return datum.getTime();
+  indexOf:function(ary, data) {
+    for (var i=0; i<ary.length;i++) {
+     if ( data == ary[i]) return i;
+    }
+    return -1;
+  }
+
+  //utility functions
+
+  dateToString:function(date) {
+    Logger.log("date: "+ datum);
+    var doc = SpreadsheetApp.getActiveSheet();
+    var datum = date;
+    Logger.log(datum);
+    return datum.getTime();
+  }
 }

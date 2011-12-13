8f9179081b64082ac38800ec563aee576f04e507
Peter Neubauer <peter@neubauer.se>
2011-12-13 22:37:17 +0100

    Update neo4j-gas-driver.js

--- neo4j-gas-driver.js
+++ neo4j-gas-driver.js
@@ -3,20 +3,20 @@ var neo4j = {
   cypher: function(query, uri) {
     var response = UrlFetchApp.fetch(uri, { "method" : "POST","payload" : query});
     return response.getContentText();
-  }
+  },
 
   cell: function(str, row, column) {
     var result=Utilities.jsonParse(str);
     var col=indexOf(result['columns'],column);
     return result['data'][row][col];
-  }
+  },
 
   indexOf:function(ary, data) {
     for (var i=0; i<ary.length;i++) {
      if ( data == ary[i]) return i;
     }
     return -1;
-  }
+  },
 
   //utility functions
 
